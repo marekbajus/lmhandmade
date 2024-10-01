105b9c0e3e5ac2e61f3bd631466da1a476684ef1
marekbajus <bajus.marek@gmail.com>
2024-10-01 12:11:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict"
+
+const resp_menu = document.getElementById("resp_menu")
+const resp_menu_open = document.getElementById("resp_menu_open")
+const resp_menu_close = document.getElementById("resp_menu_close")
+
+resp_menu_open.addEventListener("click",open_menu)
+
+function open_menu(event) {
+    event.preventDefault()
+    resp_menu.style.transform = "translate(0%,0%)"
+}
+
+resp_menu_close.addEventListener("click",close_menu)
+
+function close_menu(event) {
+    event.preventDefault()
+    resp_menu.style.transform = "translate(0%,-100%)"
+}
